172827db797752cf501eacdb16b3c96f26087088
llechi <llechi0420@gmail.com>
2024-12-11 21:55:57 +0900

    マスク画像の無駄な領域の削除プログラムの実装

--- src/afterProcessing/main.py
+++ src/afterProcessing/main.py
@@ -1,16 +1,25 @@
 # 技名, 最後の最後のフレーム番号を入力する
-from afterProcessing.annotation import Annotation
+from annotation import Annotation
 from backgroundChange import BackgroundChange
 import numpy as np
+
+# # テストしてないので，よろしく
+# waza_list = np.array([['jab_0', 100], ['jab_1', 200]])
+
+# annotation = Annotation("./../../output/images/b_0", "./../../output/images/b_0", waza_list)
+# # annotation.annotation_main()
+# annotation.get_annotation_area("01000.jpg", 0)
+
+backgroundChange = BackgroundChange("../../output/images/jabDa_0", "../../input/images/stage/resize", "../../output/images/senjou/jabDa_0")
+# backgroundChange.stage_display()
+# backgroundChange.stage_resize()
+# backgroundChange.background_change()
+
+backgroundChange.remove_small_objects()
 
 # テストしてないので，よろしく
 waza_list = np.array([['jab_0', 100], ['jab_1', 200]])
 
-annotation = Annotation("./../../output/images/b_0", "./../../output/images/b_0", waza_list)
-# annotation.annotation_main()
-annotation.get_annotation_area("01000.jpg", 0)
-
-backgroundChange = BackgroundChange("./../../input/stage_image", "./../../output/images")
-backgroundChange.stage_display()
-backgroundChange.stage_resize()
-backgroundChange.background_change()
+annotation = Annotation("../../output/images/senjou/jabDa_0", "../../output/images/senjou/jabDa_0", waza_list)
+# # annotation.annotation_main()
+annotation.get_annotation_area("1274.jpeg", 0)
