99f71aa78cb92029fc685591a147fc29659a5b08
llechi <llechi0420@gmail.com>
2024-12-25 19:51:30 +0900

    アノテーションプログラムの最終修正

--- src/afterProcessing/main.py
+++ src/afterProcessing/main.py
@@ -4,13 +4,12 @@ from backgroundChange import BackgroundChange
 import numpy as np
 
 # テストしてないので，よろしく
-waza_list = np.array([['jab_0', 100], ['jab_1', 200]])
+waza_list = np.array([['jab_0', 100], ['jab_1', 200], ['jab_2', 1458]])
 
 annotation = Annotation("../../output/images/jabDa_0/mask_image", "../../output/texts/jabDa_0/", waza_list)
-annotation.remove_small_objects()
-# annotation.annotation_main()
-annotation.get_annotation_area("1274.jpeg", 0)
+# annotation.remove_small_objects()
+annotation.annotation_main()
 
-backgroundChange = BackgroundChange("../../output/images/jabDa_0/mask_image", "../../input/images/stage/resize", "../../output/images/jabDa_0/senjou")
+# backgroundChange = BackgroundChange("../../output/images/jabDa_0/mask_image", "../../input/images/stage/resize", "../../output/images/jabDa_0/senjou")
 
-backgroundChange.run()
+# backgroundChange.run()
